d1223ec3c47de43ac200d5e78f95ff24e7fb9afe
Léo Humblot <leo.humblot@afpa-dev-pompey.fr>
2025-11-06 09:59:00 +0100

    Ajout des id html et variables javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+// Variables de jeu
+let essaisRestants = 10
+let pionsBienPlace = 0
+let pionsMalPlace = 0
+let pionsIncorrect = 0
+
+// Constantes de jeu
+const CARRE1 = document.getElementById('carre1')
+const CARRE2 = document.getElementById('carre2')
+const CARRE3 = document.getElementById('carre3')
+const CARRE4 = document.getElementById('carre4')
+const CARRE_TAB = [CARRE1, CARRE2, CARRE3, CARRE4]
+
+const PION_ROUGE = document.getElementById('pionRouge')
+const PION_BLEU = document.getElementById('pionBleu')
+const PION_VERT = document.getElementById('pionVert')
+const PION_JAUNE = document.getElementById('pionJaune')
+const BOUTON_VALIDER = document.getElementById('BoutonValider')
+const BOUTON_ANNULER = document.getElementById('boutonAnnuler')
+
+Array.from([PION_ROUGE, PION_BLEU, PION_VERT, PION_JAUNE]).forEach(element => {
+    element.addEventListener('click', function() {
+        console.log(element)
+    })
+});
