ce861794fdae16806b5d46a2336894a3723849be
Léo Humblot <leo.humblot@afpa-dev-pompey.fr>
2025-11-07 10:41:35 +0100

    Ajout de la fonction pour comparer les pions en js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // Variables de jeu
 let essaisRestants = 10;
+let pionsUtilisateur = [];
+let pionsOrdinateur = [];
 let pionsBienPlace = 0;
 let pionsMalPlace = 0;
 let pionsIncorrect = 0;
@@ -15,20 +17,77 @@ const PION_ROUGE = document.getElementById("pionRouge");
 const PION_BLEU = document.getElementById("pionBleu");
 const PION_VERT = document.getElementById("pionVert");
 const PION_JAUNE = document.getElementById("pionJaune");
+const PIONS_TAB = ["pionRouge", "pionBleu", "pionVert", "pionJaune"];
 
-const BOUTON_VALIDER = document.getElementById("BoutonValider");
+const BOUTON_VALIDER = document.getElementById("boutonValider");
 const BOUTON_ANNULER = document.getElementById("boutonAnnuler");
+
+// initialise les pions de l'ordinateur aléatoirement
+for (let i = 0; i < 4; i++) {
+    let randomIndex = Math.floor(Math.random() * 4);
+    pionsOrdinateur[i] = PIONS_TAB[randomIndex];
+}
 
 // trouve une place libre dans le tableau et ajoute la couleur à l'index
 function ajouterPion(couleur) {
     for (let i = 0; i < CARRE_TAB.length; i++) {
         if (CARRE_TAB[i].innerHTML == "") {
             CARRE_TAB[i].innerHTML =
-                '<img class="col-9 position-absolute top-50 start-50 translate-middle" src="img/pions/' +
+                '<img class="' +
+                couleur +
+                ' col-9 position-absolute top-50 start-50 translate-middle " src="img/pions/' +
                 couleur +
                 '.png" alt="" />';
             break;
         }
+    }
+}
+
+function comparePions() {
+    let pionActuel = "";
+
+    let pionsOrdinateurTemp = Array.from(pionsOrdinateur);
+
+    for (let i = 0; i < 4; i++) {
+        pionsUtilisateur[i] = CARRE_TAB[i].innerHTML;
+    }
+
+    for (let i = 0; i < 4; i++) {
+        if (pionsUtilisateur[i].includes("pionRouge")) {
+            pionActuel = "pionRouge";
+        }
+        if (pionsUtilisateur[i].includes("pionBleu")) {
+            pionActuel = "pionBleu";
+        }
+        if (pionsUtilisateur[i].includes("pionVert")) {
+            pionActuel = "pionVert";
+        }
+        if (pionsUtilisateur[i].includes("pionJaune")) {
+            pionActuel = "pionJaune";
+        }
+
+        console.log(pionActuel);
+
+        pionsOrdinateur.some(() => {
+            if (pionActuel == pionsOrdinateur[i]) {
+                pionsBienPlace++;
+                console.log(pionsOrdinateurTemp);
+                return true;
+            } else if (pionsOrdinateurTemp.includes(pionActuel.toString())) {
+                pionsMalPlace++;
+                pionsOrdinateurTemp.splice(1, pionsOrdinateurTemp.indexOf(pionActuel.toString()));
+                console.log(pionsOrdinateurTemp);
+                return true;
+            } else {
+                pionsIncorrect++;
+                console.log(pionsOrdinateurTemp);
+                return true;
+            }
+        });
+
+        console.log(pionsBienPlace);
+        console.log(pionsMalPlace);
+        console.log(pionsIncorrect);
     }
 }
 
@@ -44,4 +103,14 @@ BOUTON_ANNULER.addEventListener("click", function () {
     for (let i = 0; i < CARRE_TAB.length; i++) {
         CARRE_TAB[i].innerHTML = "";
     }
+});
+
+BOUTON_VALIDER.addEventListener("click", function () {
+    // stop la fonction si l'utilisateur n'a pas mis 4 pions
+    if (CARRE4.innerHTML == "") {
+        console.log("non");
+        return;
+    }
+
+    comparePions();
 });
